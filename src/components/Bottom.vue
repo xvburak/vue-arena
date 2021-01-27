84d85b58c2cff2aed6e15a63db616077451e52bd
xvburak <burak.vasil@gmail.com>
2021-01-27 14:08:59 +0100

    commit

--- src/components/Bottom.vue
+++ src/components/Bottom.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="bottom" class="mt-40 p-2 flex flex-wrap">
-      <p class="lg:w-3/5">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-      <p class="lg:w-2/5 mt-4 text-right self-end">info@unbranding.agency</p>
+      <p class="lg:w-3/5 self-end">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      <p class="lg:w-2/5 text-right self-end">info@unbranding.agency</p>
     </div>
 </template>
 
