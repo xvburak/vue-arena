04ab27172dd73ccd630d2ee2f939f6abe5abe02e
xvburak <burak.vasil@gmail.com>
2021-01-27 13:34:38 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <arena-images :contents="contents" />
+    <bottom />
+  </div>
+</template>
+
+<script>
+  import ArenaImages from '@/components/ArenaImages.vue'
+  import Bottom from '@/components/Bottom.vue';
+  const Arena = require("are.na");
+  const arena = new Arena();
+ 
+
+
+  export default {
+    name: 'app',
+    components: {
+      ArenaImages,
+      Bottom  
+    },
+    data(){
+      return {
+        contents: [ ],
+        options: {}
+      }
+    },
+    mounted() {
+      this.getEmployees()
+    },
+    methods: {
+        async getEmployees() {
+          // arena
+          //   .channel("unbranding-agency")
+          //   .contents({ page: 1, per: 3 })
+          //   .get()
+          //   .then(chan => {
+          //     console.log(chan.contents);
+          //     this.contents = chan.contents;
+          //     this.chan = chan;
+          //   })
+          //   .catch(err => console.log(err));
+          arena
+            .channel('unbranding-agency')
+            .contents({ page: 1, per: 100 })
+            .then(contents => {
+                console.log(contents);
+                this.contents = contents;
+                })
+            .catch(err => console.log(err));
+        },
+      },
+  }
+</script>
+
+
+<style scoped lang="scss">
+</style>
---
+++ src/components/ArenaImages.vue
@@ -0,0 +1,42 @@
+<template>
+  <Waterfall id="arena-images" class="flex flex-wrap justify-center" >
+      <WaterfallItem v-for="content in contents" :key="content.id" class="relative w-full lg:w-1/5  md:w-1/4 sm:w-1/3 p-2">
+  
+          <div v-if="content.class === 'Image'" class="rounded-md">
+            <img class="rounded-md w-full" :src="content.image.large.url" alt="">
+          </div>
+          <div v-else-if="content.class === 'Text'" class="rounded-md  bg-gray-100 p-8  md:p-4 sm:p-2 flex flex-wrap content-center">
+            <p class="w-full text-center"> {{content.content}} </p>
+          </div>
+          <div v-else-if="content.class === 'Link'" class="rounded-md">
+            <img class="rounded-md w-full" :src="content.image.large.url" alt="">
+          </div>
+          <div v-else-if="content.class === 'Media'" class="rounded-md">
+            <img class="rounded-md w-full" :src="content.image.large.url" alt="">
+          </div>
+          <div v-else>
+            <p>{{content.class}}</p>
+          </div>
+          <!-- <p>{{content.title}}</p> -->
+      </WaterfallItem>
+    </Waterfall>
+</template>
+
+<script>
+   const Waterfall = require("vue2-waterfall").Waterfall;
+   const WaterfallItem = require("vue2-waterfall").WaterfallItem;
+    
+  export default {
+    name: 'arena-images',
+    components: {
+      Waterfall,
+      WaterfallItem
+    },
+    props: {
+      contents: Array,
+    },
+  }
+</script>
+
+<style scoped>
+</style>
